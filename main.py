a780f482c61ad38fcc0c485256efd3cd2a3c7aa3
Nikait <50284221+Nikait@users.noreply.github.com>
2024-04-30 14:24:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import hydra
 import torch
+import time
+import datetime
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
 
@@ -39,17 +41,27 @@ def main(cfg):
         lr=cfg["train"]["lr"]
     )
 
+    start = time.time()
+
     for epoch in range(cfg["train"]["epoch_count"]):
         # fit
+        print("_"*50)
+        print("current parameters: \n")
+        for name, param in model.state_dict().items():
+            print(name, param, sep="\n")
+        print("_"*50)
         epoch_history = []
         for i, (x, y) in enumerate(train_dataloader):
             opt.zero_grad()
             loss = model(x, y)
             loss.backward()
             opt.step()
+            end = datetime.timedelta(seconds=time.time()-start)
+            print(f"epoch: {epoch+1} | batch: {i+1:2d} | "
+                  f"loss: {loss[0]:.3f} | current time: {end}")
             epoch_history.append(loss[0])
         
-        loss = max(epoch_history) / len(epoch_history)
+        loss = sum(epoch_history) / len(epoch_history)
 
         # predict
         x, y = next(iter(test_dataloader))
@@ -58,8 +70,10 @@ def main(cfg):
 
         losses.append(float(loss))
         scores.append(score)
-
-        print(f"epoch: {epoch} | loss: {loss:.3f} | accuracy: {score:.3f}")
+        
+        end = datetime.timedelta(seconds=time.time()-start)
+        print(f"[*] epoch: {epoch+1:2d} | loss: {loss:.3f} | "
+              f"accuracy: {score:.3f} | current time: {end}")
 
     plot_history(cfg["plot"]["path"], losses, scores)
     print("History has been plotted to", cfg["plot"]["path"])
