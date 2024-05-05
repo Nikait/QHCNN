014581cb70034a8fa31bc80990dcc47ef16d4b75
Nikait <50284221+Nikait@users.noreply.github.com>
2024-05-05 17:49:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,81 +2,85 @@ import hydra
 import torch
 import time
 import datetime
+import logging
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
 
 from dataset import MNISTDataset
 from model import QCNN
 from plotting import plot_history
+from conf.structured_config import Config
 
 
-@hydra.main(config_path="conf", config_name="config", version_base="1.3.2")
-def main(cfg):
-    train_dataset = MNISTDataset(
-        *cfg["data"].values(),
-        train=True
-    )
-    
-    test_dataset = MNISTDataset(
-        *cfg["data"].values(),
-        train=False
-    )
-
+@hydra.main(config_name="config", version_base="1.3.2")
+def main(cfg: Config) -> None:
+    logging.basicConfig(level=logging.INFO)
     train_dataloader = DataLoader(
-        dataset=train_dataset,
-        batch_size=cfg["train"]["batch_size"]
+        dataset = MNISTDataset(
+            *cfg.data.values(),
+            train=True
+        ),
+        batch_size = cfg.train.batch_size,
+        shuffle = True
     )
 
     test_dataloader = DataLoader(
-        dataset=test_dataset,
-        batch_size=cfg["data"]["min_length"]
+        dataset = MNISTDataset(
+            *cfg.data.values(),
+            train=False
+        ),
+        batch_size = cfg.data.min_length,
+        #shuffle = True
     )
 
     losses, scores = [], []
 
-    model = QCNN()
+    model = QCNN(cfg)
 
     opt = torch.optim.Adam(
         model.parameters(), 
-        lr=cfg["train"]["lr"]
+        lr=cfg.train.lr
+    )
+
+    scheduler = torch.optim.lr_scheduler.LinearLR(
+        opt,
+        start_factor=cfg.train.start_factor,
+        end_factor=cfg.train.end_factor,
+        total_iters=cfg.train.epoch_count
     )
 
     start = time.time()
 
-    for epoch in range(cfg["train"]["epoch_count"]):
+    for epoch in range(cfg.train.epoch_count):
         # fit
-        print("_"*50)
-        print("current parameters: \n")
-        for name, param in model.state_dict().items():
-            print(name, param, sep="\n")
-        print("_"*50)
         epoch_history = []
         for i, (x, y) in enumerate(train_dataloader):
             opt.zero_grad()
             loss = model(x, y)
             loss.backward()
             opt.step()
-            end = datetime.timedelta(seconds=time.time()-start)
-            print(f"epoch: {epoch+1} | batch: {i+1:2d} | "
-                  f"loss: {loss[0]:.3f} | current time: {end}")
+            end = str(datetime.timedelta(seconds=time.time()-start))
             epoch_history.append(loss[0])
         
+        scheduler.step()
         loss = sum(epoch_history) / len(epoch_history)
 
         # predict
         x, y = next(iter(test_dataloader))
         y_pred = model.predict(x)
         score = accuracy_score(y, y_pred)
+        end = str(datetime.timedelta(seconds=time.time()-start))
 
         losses.append(float(loss))
         scores.append(score)
-        
-        end = datetime.timedelta(seconds=time.time()-start)
-        print(f"[*] epoch: {epoch+1:2d} | loss: {loss:.3f} | "
-              f"accuracy: {score:.3f} | current time: {end}")
 
-    plot_history(cfg["plot"]["path"], losses, scores)
-    print("History has been plotted to", cfg["plot"]["path"])
+        logging.info(
+            f"epoch: {epoch+1:2d} | loss: {loss:.3f} | "
+            f"accuracy: {score:.3f} | current time: {end:.7}"
+        )
+
+    plot_history(cfg.plot.path, losses, scores)
+    logging.info("History has been plotted to", cfg.plot.path)
 
 
 if __name__ == "__main__":
